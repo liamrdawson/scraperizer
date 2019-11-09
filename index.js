2df09a55e09f0785d7a51d3f5b7a81ed5cbd55a1
Liam Dawson <liamrdawson@gmail.com>
2019-11-09 00:39:57 +0000

    return html from getHtml function using axios

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {getHtml} from './lib/scraper';
 
 async function startScrape() {
-    console.log(await getHtml());
+    console.log(await getHtml(`https://twitter.com/LiamRDawson`));
 }
 
 startScrape();
