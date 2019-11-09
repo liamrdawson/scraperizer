a002c683dceffa86034a9fba2174c9998d5d65fe
Liam Dawson <liamrdawson@gmail.com>
2019-11-09 21:02:36 +0000

    clean up startScrape to display twitter count and instagram count in same log

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import {getHtml, getTwitterFollowers, getInstagramFollowers} from './lib/scraper
 async function startScrape() {
     const html  = await getHtml(`https://twitter.com/LiamRDawson`);
     const twCount = await getTwitterFollowers(html);
-    console.log(`You have ${twCount} followers on Twitter.`); 
     const instaCount = await getInstagramFollowers();
-    console.log(`You have ${instaCount} followers on Instagram.`);
+    console.log(`You have ${instaCount} followers on Instagram and ${twCount} followers on Twitter.`);
     
 }
 
