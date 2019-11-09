372e309f043884c0cedd1d5b48a693fc8562ea51
Liam Dawson <liamrdawson@gmail.com>
2019-11-09 20:53:52 +0000

    getInstagram followers returns count of followers by using the Instagram API with variable username

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import {getHtml, getTwitterFollowers} from './lib/scraper';
+import {getHtml, getTwitterFollowers, getInstagramFollowers} from './lib/scraper';
 
 async function startScrape() {
     const html  = await getHtml(`https://twitter.com/LiamRDawson`);
     const twCount = await getTwitterFollowers(html);
     console.log(`You have ${twCount} followers on Twitter.`); 
+    const instaCount = await getInstagramFollowers();
+    console.log(`You have ${instaCount} followers on Instagram.`);
+    
 }
 
 startScrape();
