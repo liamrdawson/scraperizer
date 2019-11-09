2624eacfd232fc3107e24747e4f017b942b6f2a7
Liam Dawson <liamrdawson@gmail.com>
2019-11-09 21:07:07 +0000

    move html logic over to getTwitterFollowers so now bot getXFollowers functions only take username as param

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import {getHtml, getTwitterFollowers, getInstagramFollowers} from './lib/scraper';
 
 async function startScrape() {
-    const html  = await getHtml(`https://twitter.com/LiamRDawson`);
-    const twCount = await getTwitterFollowers(html);
+    const twCount = await getTwitterFollowers();
     const instaCount = await getInstagramFollowers();
     console.log(`You have ${instaCount} followers on Instagram and ${twCount} followers on Twitter.`);
     
