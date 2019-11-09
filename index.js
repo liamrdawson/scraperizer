34dbf7fb777449e656ee6207f402ab04d24f33bd
Liam Dawson <liamrdawson@gmail.com>
2019-11-09 21:10:37 +0000

    set usernames for twitter and instagram as default at startScrapeLevel, data returned now depends on params provided at top level function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {getHtml, getTwitterFollowers, getInstagramFollowers} from './lib/scraper';
 
-async function startScrape() {
-    const twCount = await getTwitterFollowers();
-    const instaCount = await getInstagramFollowers();
+async function startScrape(twitUser = 'LiamRDawson', instaUser = 'liam_r_dawson') {
+    const twCount = await getTwitterFollowers(twitUser);
+    const instaCount = await getInstagramFollowers(instaUser);
     console.log(`You have ${instaCount} followers on Instagram and ${twCount} followers on Twitter.`);
     
 }
