35a4c8672dfc4046276be8083ee0663c01cfd765
Liam Dawson <liamrdawson@gmail.com>
2019-11-11 22:52:51 +0000

    Save data to DB

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 # **To Do List**
 
 - [x] Set up Express server
-- [ ] Save data to DB
+- [x] Save data to DB
 - [ ] Setup a cron job
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import db from './lib/db';
 import {getTwitterFollowers, getInstagramFollowers} from './lib/scraper';
 
 const app = express();
+
+console.log(db);
 
 app.get('/scrape', async (req, res, next) => {
     console.log(`Scraping!`);
@@ -10,7 +13,21 @@ app.get('/scrape', async (req, res, next) => {
         getTwitterFollowers()
     ]);
     console.log(instaCount, twitCount);
-    res.json({instaCount, twitCount})
+    res.json({instaCount, twitCount});
+
+    db.get('twitter').push(
+        {
+            date: Date.now(),
+            count: twitCount
+        }
+    ).write();
+
+    db.get('instagram').push(
+        {
+            date: Date.now(),
+            count: instaCount
+        }
+    ).write();
 });
 
 app.listen(8652, () => console.log(`example app running on port 8652`));
